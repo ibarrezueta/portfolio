1f2e9e06fa2d6064b46e44c79f7cd2afe99a2151
barrezuetai <barrezuetai@allegheny.edu>
2019-06-16 15:34:03 -0400

    Changed SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Isaac Barrezueta's Portfolio`,
+    description: `Using gatsby and react.js to hold all of my projects`,
+    author: `Isaac Barrezueta`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
